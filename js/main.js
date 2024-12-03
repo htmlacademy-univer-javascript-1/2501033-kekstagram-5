e1bf12d7284e48380dafe7825672f9ab6a8c0848
Байдуганов Александр <albdgnv@gmail.com>
2024-12-03 17:48:34 +0500

    9.15. Помощь друга

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ import './util.js';
 import './data.js';
 import render from './thumbnails-render.js';
 import './form-validation.js';
+import {initImageEditor} from './image-editor.js';
 
 
 render();
+initImageEditor();
